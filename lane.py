7af5a4dd0bdace2fc9c22a8340e1fd2bb5e8c496
Syed-Rafi-Naqvi <sd.rafi1819@gmail.com>
2019-02-13 22:17:15 +0530

    video added and lane.py updated

--- lane.py
+++ lane.py
@@ -3,6 +3,36 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+def make_coordinate(image, line_parameters):
+    slope, intercept = line_parameters
+    y1 = image.shape[0]
+    y2 = int(y1*(3/5))
+    x1 = int((y1 - intercept)/slope)
+    x2 = int((y2 - intercept)/slope)
+    return np.array([x1, y1, x2, y2])
+    
+    
+    
+def average_lines(image, lines):
+    right = []
+    left = []
+    for line in lines:
+        x1, y1, x2, y2 = line.reshape(4)
+        parameters = np.polyfit((x1, x2), (y1, y2), 1)
+        slope = parameters[0]
+        intercept = parameters[1]
+        if slope < 0:
+            left.append((slope, intercept))
+        else:
+            right.append((slope, intercept))
+    left_avg = np.average(left, axis = 0)
+    right_avg = np.average(right, axis = 0)
+    left_line = make_coordinate(image, left_avg)
+    right_line = make_coordinate(image, right_avg)
+    return np.array([left_line, right_line])
+
+
+
 def canny(image):
     grayImg1 = cv.cvtColor(image, cv.COLOR_RGB2GRAY)
     blur = cv.GaussianBlur(grayImg1, (5,5), 0)
@@ -21,8 +51,7 @@ def region_of_interest(image):
 def display_lines(image, lines):
     line_image = np.zeros_like(image)
     if lines is not None:
-        for line in lines:
-            x1, y1, x2, y2 = line.reshape(4)
+        for x1, y1, x2, y2 in lines:
             cv.line(line_image, (x1,y1), (x2,y2), (0,255,0), 10)
     return line_image
 
@@ -32,8 +61,10 @@ grayImg = np.copy(img)
 canny = canny(grayImg)
 roi = region_of_interest(canny)
 lines = cv.HoughLinesP(roi, 2, np.pi/180, 100, np.array([]), minLineLength = 40, maxLineGap = 5 )
-line_image = display_lines(grayImg, lines)
+averaged_lines = average_lines(grayImg, lines)
+line_image = display_lines(grayImg, averaged_lines)
 added_image = cv.addWeighted(grayImg, 0.8, line_image, 1, 1)
+
 
 
 cv.imshow('canny', canny)
